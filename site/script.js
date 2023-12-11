637839b404aac694764b1f8c380cb7ecdc3839b7
Mohammed Rehaan <73173381+mohammed-rehaan@users.noreply.github.com>
2023-12-11 12:49:25 +0530

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,5 +1,7 @@
 console.log("Choo choo!");
 
 if(window.location.href.split('/') > 1){
-  window.location.href = window.location.href.split('/')[0]
+  console.log("SPLIT",window.location.href)
+  console.log("TEST", window.location.href.split('/') > 1)
+  window.location.href = 'https://expenso-4y16.onrender.com/'
 }
